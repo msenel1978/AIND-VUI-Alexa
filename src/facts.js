b1d60d05752b14b259099be19348551d120252d1
Murat Senel <msenel@Murats-MacBook-Pro.local>
2018-03-26 01:57:38 -0400

    Initial commit: Part 1 done Done - Part 1: Customize the fact skill

--- src/facts.js
+++ src/facts.js
@@ -7,6 +7,15 @@ module.exports = {
         "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
         "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
         "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "The field of artificial intelligence research was founded as an academic discipline in 1956.",
+        "On 11 May 1997, Deep Blue became the first computer chess-playing system to beat a reigning world chess champion, Garry Kasparov.",
+      	"In March 2016,  AlphaGo, an AI system built by DeepMind has won four of five games agains South Korean 'Go' champion Lee Sedol.",
+      	"Microsoft launched Kinect for Xbox 360, the first gaming device to track human body movement, this inovation happend in 2010.",
+      	"In 2009, Google built their first Self-driving car.",
+      	"A semi-autonomous car drove coast-to-coast across the United States with computer-controlled steering, this happend in 1995.",
+      	"In 1980, first National Conference of the American Association for Artificial Intelligence (AAAI) held at Stanford.",
+      	"In 1985, Neural Networks become widely used with the Backpropagation algorithm.",
+      	"2004 was big for AI community, NASA's robotic exploration rovers, Spirit and Opportunity, autonomously navigate the surface of Mars.",
+      	"In 2011, IBM's Watson computer defeated television game show Jeopardy! champions Rutter and Jennings."
     ]
 }
